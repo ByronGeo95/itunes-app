66dd743ee24a7d2c57f50c2e6ba4596ee4aeca18
Byron <byrongeo95@gmail.com>
2020-09-16 15:48:56 +0200

    fix

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ fetch(`${apiURL}`)
 });
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('itunes-api-react/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'itunes-api-react', 'build', 'index.html'));
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
 
 }
